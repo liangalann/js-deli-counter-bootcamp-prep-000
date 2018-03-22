9f8ac9f2cee896cfa6fa977b8c5242048ef39b9b
Alan Liang <liangalann@gmail.com>
2018-03-22 21:46:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,9 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  if(!katzDeliLine.length){
-    return "The line is currently empty.";
+
+  if (!katzDeliLine.length) {
+    return "The line is currently empty."
   }
-  var lines = [];
-  for (var i=0; i < line.length; i++) {
-    lines.push(i + 1 + ". " + line[i]);
-  }
-  console.log("The line is currently: " + lines)
-  return "The line is currently: " + lines.join(', ');
 }
+currentline()
